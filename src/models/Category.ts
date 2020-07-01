252c6a48a4a7a855d9a8f8509d8eeedc657a300f
José Assis de Menezes Neto <netomenezesucg@gmail.com>
2020-06-30 22:14:26 -0300

    Finalizado segundo desafio do nível 2 do bootcamp GoStack12

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,11 +1,18 @@
+import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+
+@Entity('categories')
 class Category {
-  id: string;
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
 
-  title: string;
+    @Column()
+    title: string;
 
-  created_at: Date;
+    @CreateDateColumn()
+    created_at: Date;
 
-  updated_at: Date;
+    @UpdateDateColumn()
+    updated_at: Date;
 }
 
 export default Category;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,11 +1,53 @@
-// import AppError from '../errors/AppError';
+import {getCustomRepository, getRepository} from "typeorm";
 
+import AppError from '../errors/AppError';
+import TransactionsRepository from "../repositories/TransactionsRepository";
 import Transaction from '../models/Transaction';
+import Category from "../models/Category";
+
+interface Request {
+    title: string;
+    type: 'income' | 'outcome';
+    value: number;
+    category: string
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
-  }
+    public async execute({title, value, type, category}: Request): Promise<Transaction> {
+        const transactionRepository = getCustomRepository(TransactionsRepository);
+        const categoryRepository = getRepository(Category);
+
+        const { total } = await transactionRepository.getBalance();
+
+        if (type === 'outcome' && total < value) {
+            throw new AppError('You do not have enough balance');
+        }
+
+        let categoryTransaction = await categoryRepository.findOne({
+            where: {
+                title: category
+            }
+        });
+
+        if (!categoryTransaction) {
+            categoryTransaction = categoryRepository.create({
+                title: category
+            });
+
+            await categoryRepository.save(categoryTransaction);
+        }
+
+        const transaction = transactionRepository.create({
+            title,
+            value,
+            type,
+            category: categoryTransaction
+        });
+
+        await transactionRepository.save(transaction);
+
+        return transaction;
+    }
 }
 
 export default CreateTransactionService;
